808e8e25427be768ad062894ca10e5d5a4592713
Samantha Skurnick <sa286446@ucf.edu>
2020-01-29 12:14:42 -0500

    Co-authored-by: Graceface0523 <Grace18462@gmail.com> Co-authored-by: sfarls <sfarls2018@knights.ucf.edu> Co-authored-by: Jenny <j.rongjie@gmail.com>

--- ice/w4/src/index.js
+++ ice/w4/src/index.js
@@ -2,7 +2,7 @@ import Component from "./Component.js";
 import Task from "./Task.js";
 function runOnLoad()
 {
-    
+    console.log("New change");
     let element = document.createElement("div");
     element.id = "Test";
     document.body.appendChild(element);
@@ -11,6 +11,16 @@ function runOnLoad()
     let element2 = document.createElement("div");
     element2.innerHTML = comp.render();
     console.log("Hello World truly");
-}
+    let myTask = new Task({
+        content: "Finish ICE Exercise",
+        done: false
+    }
+   )
+   var container = document.getElementById("taskList");
+   var newtask = document.createElement("div");
+   newtask.innerHTML = myTask.render();
+   container.appendChild(newtask);
+   console.log(myTask.render());
+    }
 
 window.addEventListener("DOMContentLoaded", runOnLoad);
