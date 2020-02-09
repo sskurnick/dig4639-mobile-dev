a31e2deb4aeec6d0e75ade805c76f2e08d50a802
sskurnick <sskurnick@knights.ucf.edu>
2020-02-09 17:41:50 -0500

    lab 2

---
+++ labs/2/src/components/Card/index.js
@@ -0,0 +1,24 @@
+import "./index.css"
+
+class Card {
+    set component(component) {
+      this.component = component;
+      this.props = props;
+    }
+    get component() {
+      return this.component;
+      return this.props;
+    }
+    constructor(elementTag) {
+      this.element = document.createElement(elementTag);
+      document.body.appendChild(this.element);
+    }
+  
+    render() {
+      this.element.innerHTML = this.component.render();
+    }
+  
+  }
+
+export default Card;
+
