559aeeebb324d913e40eb7ab1ba505d4b08ca19d
sskurnick <sskurnick@knights.ucf.edu>
2020-02-23 15:22:47 -0500

    Lab 3

---
+++ labs/3/my-app/src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Card from './components/Card';
+import './App.css';
+
+function App() {
+  return (
+   <Card content="This is a card!"></Card>
+  );
+}
+  
+export default App;
